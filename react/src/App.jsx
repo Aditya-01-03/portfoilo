f8acacbbfc02dc0fb59485ec68b6328c84995b03
Aditya Bhone <147229617+Aditya-01-03@users.noreply.github.com>
2025-07-22 22:22:50 +0530

    Second commit

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  const abc = () =>{
+    console.log("Hello WOrld")
+  }
+ return <div>
+ <h1>I am Aditya Bhone</h1> 
+ <button onClick={abc}>Click me</button>
+ </div>
+}
+
+export default App
